ca5fc561c9928a442a87f6a46443ea19e91a288c
Timothy Ball <timball@hillaryclinton.com>
2015-12-21 20:00:29 -0500

    be incorrect

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 REQUIREMENTS = [
-        'requests',
-        'lxml'
+        'requests'
 ]
 
 setup(
